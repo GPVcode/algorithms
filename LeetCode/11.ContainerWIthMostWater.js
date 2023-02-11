f7164f3ec2c365864f44c635d20291034996d48f
GPV <gvill005@gmail.com>
2023-02-11 12:31:28 -0800

    updated brute force method for leetcode 11.COntainerWi...

--- LeetCode/11.ContainerWIthMostWater.js
+++ LeetCode/11.ContainerWIthMostWater.js
@@ -20,39 +20,41 @@
  * 
  */
 
+// BRUTE FORCE APPROACH
 /* 
     Understand the Problem:
         Find the indices whose values create the largest area. Output the largest area.
     Devise a Plan:
         Brute Force
         Use nested loops to find area between two indexes and its values. 
-        use a holder variable to store current highest number. at end of loops last number remains
-        return highest number
-    Code:
+        use Math.min and Math.max to find the value we are looking for to plug into area formulal
+        return maxArea
+    Code: Brute Force
                                                          i
                                         Input: height = [1,8,6,2,5,4,8,3,7]
                                                              j
 Output: 49
 */ 
-// Brute Force
-const maxArea = function(height) {
+const maxArea4 = function(height) {
     //create max area holder
     let maxArea = 0;
     //use nested loops to work with values
     for(let i = 0; i < height.length; i++){
         for(let j = i + 1; j < height.length; j++){
-            let area1 = (j - i) * height[i];
-            let area2 = (j - 1) * height[j];
-            if(height[i] <= height[j] && area1 > maxArea){
-                maxArea = area1; //maxArea = 1
-            }
-            else if(height[i] > height[j] && area2 > maxArea){
-                maxArea = area2
-            }
+            const base = j - i;
+            const useHeight = Math.min(height[i], height[j]);
+            const area = base * useHeight;
+            maxArea = Math.max(maxArea, area);
         }
     }
     //return max Area
     return maxArea;
 };
+console.log("maxArea4:", maxArea4([1,1]))
 
-console.log("maxArea:", maxArea([1,1]))
+/*
+Understand the Problem:
+
+Devise a plan:
+
+*/
