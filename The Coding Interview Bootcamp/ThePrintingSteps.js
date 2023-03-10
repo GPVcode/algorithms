b9f5a4512d00fd167d593b8f7324bc2c9ce83fb2
GPV <gvill005@gmail.com>
2023-03-10 01:51:15 -0800

    solved printing steps using recursion, brute force, and array

--- The Coding Interview Bootcamp/ThePrintingSteps.js
+++ The Coding Interview Bootcamp/ThePrintingSteps.js
@@ -46,4 +46,112 @@ function steps(n) {
     }
 }
 
-console.log("steps:", steps(4)) 
+// console.log("steps:", steps(4)) 
+
+/*
+Core Understanding:
+    return stairs using "#" character while maintaining length;
+Devising a Plan:
+    Think of the solution as a matrix
+     jjjj
+    i#___ 
+    i##__
+    i###_
+    i####
+    use nested loops to fill up matrix
+    start with an empty string
+        if j is equal to or less than i, add "#"
+        otherwise " ""
+    console.log the now not so empty string
+Code it Out: Brute Force
+*/
+function steps2(n) {
+    //use nested loops
+    for(let i = 0; i < n; i++){
+        // create empty string
+        let stair = "";
+        for(let j = 0; j < n; j++){
+            //if condition to trigger adding "#"
+            if(j <= i) stair += "#";
+            // else add space
+            else stair += " ";
+        }
+        console.log("stair:", stair)
+    } 
+}
+
+// console.log("steps2:", steps2(4))
+
+/*
+Core Understanding:
+    return stairs using "#" character while maintaining length;
+Devising a Plan:
+    set row to 0 and stair to empty string as paramenters
+    set base case where row equals n input
+    to move row, check if length of stair string equals n
+        use recursion to rerun function and add 1 row
+    use another if condition to check if space or '#' should be added
+    call recursive function again
+Code it Out: Recursion
+*/
+function printNumber(n, row = 0, stair = '') {
+    // set base case
+    if(n === row)return; //if(row === n) then we have hit the end of our problem
+
+    //if the 'stair' string has a length === n then we are at the end of a row
+    if(stair.length === n){
+        console.log(stair);
+        return printNumber(n, row + 1);
+    } 
+    //if the length of the stair string is less than or equal to the row number we're working on, we add a '#'
+    if(stair.length <= row ){
+        stair += "#";
+    } else{ //otherwise add space
+        stair += ' '; 
+    }
+    printNumber(n, row, stair);
+}
+
+printNumber(4);
+/*
+Core Understanding:
+    return stairs using "#" character while maintaining length;
+Devising a Plan:
+    Think of the solution as a matrix
+     jjjj
+    i#___ 
+    i##__
+    i###_
+    i####
+    first use loop to create empty array
+    use two pointers to fill up matrix
+    use while loop to iterate
+        if poointer 1 is equal to or less than pointer 2, add "#"
+        otherwise " "
+        console.log array as string
+Code it Out: Two-Pointer
+*/
+// function steps3(n) {
+//     // create empty array
+//     let stair = "";
+
+//     //x and y matrix
+//     let x = 0, y = 0; // x = 4 y = 3
+//     while(y < n){
+//         if(x <= y){
+//             stair += "#";
+//         } if(x > y){
+//             stair += "space";
+//             x++;
+//         } if(x === n){
+//             console.log("stairs:", stair)
+//             y++;
+//             x = 0;
+//             stair = ""
+//         }
+//         x++
+//     }
+    
+// }
+
+// console.log("steps3:", steps3(4)) 
