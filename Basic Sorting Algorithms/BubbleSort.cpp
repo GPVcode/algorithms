8636116b6de01c558bf1ff34c9f5f45c587bf9c4
GPV <gvill005@gmail.com>
2024-10-13 17:07:15 -0700

    working on fundamentals starting

---
+++ Basic Sorting Algorithms/BubbleSort.cpp
@@ -0,0 +1,20 @@
+// Bubble Sort Algorithm - the simplest sorting algorithm
+// Although inefficient, bubble sort is intuitive and introduces the basic concept of comparison and swapping.
+#include <iostream>
+
+using namespace std;
+
+class BubbleSort{
+    public:
+
+        void sort(){
+
+        }
+};
+
+int main(){
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+
+    //call bubble sort
+
+};
